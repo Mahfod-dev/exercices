4af61a600d553d8d94ae458d87daa68721edd0f8
Mahfod <mahfod.dev@gmail.com>
2023-01-25 16:28:44 +0100

    exercice TS

---
+++ exercice2/script.js
@@ -0,0 +1,15 @@
+
+
+
+
+const Pet = (props) => {
+	
+};
+
+const App = () => {
+
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
